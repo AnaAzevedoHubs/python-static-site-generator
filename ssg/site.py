de3d3afde2a20fc20c186a572bd80ec331422422
AnaAzevedo <ana.azevedo@3dhubs.com>
2021-02-05 15:38:04 +0100

    1.2 v2

--- ssg/site.py
+++ ssg/site.py
@@ -2,5 +2,5 @@ import pathlib
 
 class Site:
     def __init__(self, source, dest) -> None:
-        self._source = pathlib.Path(source)
-        self._dest = pathlib.Path(dest)
+        self.source = pathlib.Path(source)
+        self.dest = pathlib.Path(dest)
